6a4198779ef4aa14feee516cd1fafa041c56a6e5
Josh Hoover <caboodlennm@gmail.com>
2023-05-13 09:16:22 -0500

    More text editor stuff

--- src/components/PromptInput.tsx
+++ src/components/PromptInput.tsx
@@ -1,12 +1,15 @@
-import { use, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FeedbackLevel, colorFromFeedbackLevel } from "~/lib/feedback";
 import Graphemer from "graphemer";
+import { compile } from "html-to-text";
 
 import { get_encoding } from "@dqbd/tiktoken";
 
 const encoder = get_encoding("cl100k_base");
 const textDecoder = new TextDecoder();
 const graphemer = new Graphemer();
+
+const compiledConvert = compile({ wordwrap: false, preserveNewlines: true });
 
 export function getSegments(inputText: string) {
   const encoding = encoder.encode(inputText, "all");
@@ -37,13 +40,28 @@ export function getSegments(inputText: string) {
   return segments;
 }
 
+const colorFromIndex = (index: number) => {
+  switch (index % 3) {
+    case 0:
+      return " bg-blue-300";
+    case 1:
+      return " bg-green-300";
+    case 2:
+      return " bg-yellow-300";
+  }
+};
+
 type HoverableTextProps = {
   text: string;
   mousePosition: { x: number; y: number };
+  index: number;
+  setInfoToken: (token: string | null) => void;
 };
 const HoverableText: React.FC<HoverableTextProps> = ({
   text,
   mousePosition,
+  index,
+  setInfoToken,
 }) => {
   const spanRef = useRef<HTMLSpanElement>(null);
 
@@ -64,15 +82,45 @@ const HoverableText: React.FC<HoverableTextProps> = ({
       mousePosition.y <= bottomRight.y;
 
     setHovered(isHovered);
+    if (isHovered) {
+      setInfoToken(text);
+    }
   }, [spanRef, mousePosition]);
 
   return (
-    <span
-      ref={spanRef}
-      className={"rounded-sm" + (hovered ? " bg-blue-300 bg-opacity-40" : "")}
-    >
-      {text}
-    </span>
+    <>
+      <span
+        ref={spanRef}
+        className={
+          "rounded-sm " +
+          colorFromIndex(index) +
+          (hovered ? " bg-red-400 bg-opacity-60" : " bg-opacity-40")
+        }
+      >
+        {text}
+      </span>
+      {text === "\n" ? <br /> : null}
+    </>
+  );
+};
+
+const flattenSpanText = (html: string) => {
+  // remove span tags
+  const noSpan = html.replace(/<span[^>]*>/g, "").replace(/<\/span>/g, "");
+  return noSpan;
+};
+
+type TokenInfoProps = {
+  token: string;
+};
+const TokenInfo: React.FC<TokenInfoProps> = ({ token }) => {
+  const encoding = encoder.encode(token, "all");
+
+  return (
+    <div className="w-48 flex flex-col justify-start gap-2">
+      <h3 className="text-lg font-bold">"{token}"</h3>
+      <h3 className="text-lg font-bold">Encoding: {encoding.join(", ")}</h3>
+    </div>
   );
 };
 
@@ -89,60 +137,83 @@ const PromptInput: React.FC<PromptInputProps> = ({
   // we will have a hidden contenteditablediv that will be edited and then we will wrap each character when we display it
 
   const editableRef = useRef<HTMLDivElement>(null);
-  const overlayRef = useRef<HTMLDivElement>(null);
 
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
-  const [segments, setSegments] = useState<ReturnType<typeof getSegments>>(
-    []
-  );
+  const [segments, setSegments] = useState<ReturnType<typeof getSegments>>([]);
+  const [infoToken, setInfoToken] = useState<string | null>(null);
 
   useEffect(() => {
     const segments = getSegments(prompt);
-    console.log("segments", segments);
+    // console.log("segments", segments);
     setSegments(segments);
+    setInfoToken(null);
+    // check if the prompt has newlines
+    if (/\n/g.test(prompt)) {
+      console.log("newlines detected");
+    }
   }, [prompt]);
+
+  const [content, setContent] = useState<string>("");
 
   return (
     <div className="flex flex-row items-center justify-center gap-4">
-      <div className="relative h-16 w-96">
+      <div className="relative h-16 w-96 text-black">
         <div className="pointer-events-none absolute left-0 top-0 z-10 h-full w-full rounded-md border-2">
-          {/* {prompt.split("").map((char, i) => (
-            <HoverableText key={i} text={char} mousePosition={mousePosition} />
-          ))} */}
           {segments.map((segment, i) => (
             <HoverableText
               key={i}
+              index={i}
               text={segment.text}
               mousePosition={mousePosition}
+              setInfoToken={setInfoToken}
             />
           ))}
         </div>
         <div
-          className="absolute left-0 top-0 h-full w-full rounded-md border-2 border-gray-300 bg-red-200 text-black"
+          className="absolute left-0 top-0 h-full w-full rounded-md border-2 border-gray-300 bg-gray-200"
           contentEditable={true}
           suppressContentEditableWarning={true}
           onInput={(e) => {
-            setPrompt(e.currentTarget.innerText);
+            const text = compiledConvert(e.currentTarget.innerText);
+            // Idk if this makes any sense
+            console.log("text", text);
+            setPrompt(text);
+            // if (e.currentTarget.innerHTML !== text) {
+            //   const replacement = text.replace(/\n/g, "<br/>");
+            //   console.log("replacing", e.currentTarget.innerHTML, replacement);
+            //   e.currentTarget.innerText = replacement;
+            // }
+            console.log("here");
+            const replaced = flattenSpanText(e.currentTarget.innerHTML);
+            if (replaced !== e.currentTarget.innerHTML) {
+              console.log("replacing", replaced, text);
+              e.currentTarget.innerHTML = text;
+            }
           }}
           ref={editableRef}
           onClick={(e) => {
             console.log("editable click", e);
           }}
           onMouseMove={(e) => {
+            setInfoToken(null);
             setMousePosition({ x: e.clientX, y: e.clientY });
           }}
         ></div>
       </div>
-      <button
-        className={
-          "rounded-lg px-4 py-2" +
-          colorFromFeedbackLevel(FeedbackLevel.Success, true)
-        }
-        onClick={onSubmit}
-      >
-        Submit
-      </button>
+      <div className="flex flex-col items-center justify-center gap-2">
+        <h2 className="text-2xl">Token Count: {segments.length}</h2>
+        <TokenInfo token={infoToken ?? ""} />
+        <button
+          className={
+            "rounded-lg px-4 py-2" +
+            colorFromFeedbackLevel(FeedbackLevel.Success, true)
+          }
+          onClick={onSubmit}
+        >
+          Submit
+        </button>
+      </div>
     </div>
   );
 };
