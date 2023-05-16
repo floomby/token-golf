3f19a217a7b22f87b79c36dbe310aaff056b8839
Josh Hoover <caboodlennm@gmail.com>
2023-05-16 18:00:53 -0500

    Bugfix test component

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -38,16 +38,24 @@ const Test: React.FC<TestProps> = ({ test, expected, result, className }) => {
       {!!result && (
         <div
           className={
-            "rounded-lg bg-slate-300 p-2 font-semibold text-black dark:text-white" +
+            "grid w-full grid-cols-4 gap-2 rounded-lg bg-slate-300 p-2 font-semibold text-black dark:text-white " +
             (result.success
               ? " border-green-500 bg-green-200 dark:bg-green-900"
               : " border-red-500 bg-red-200 dark:bg-red-900")
           }
         >
-          Received Output:{" "}
-          <span className="rounded-sm border-2 border-gray-500 p-1 font-mono">
-            {expected}
-          </span>
+          <span className="col-span-1">Produced Output</span>
+          <div className="col-span-3">
+            <code
+              className="whitespace-pre-wrap rounded-sm border-2 border-gray-500 p-1"
+              style={{
+                wordBreak: "break-word",
+                display: "block",
+              }}
+            >
+              {result.result}
+            </code>
+          </div>
         </div>
       )}
     </div>
